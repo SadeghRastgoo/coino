a318793248096a599ed12d03ce72d1c5ea0b377b
Sadegh Rastgoo <sadegh.rsg@gmail.com>
2023-01-04 17:41:00 +0330

    💄 add ArrowUpIcon and ArrowDownIcon

---
+++ src/components/Icons/ArrowDownIcon.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    stroke-width="3"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="feather feather-trending-down h-full w-3 stroke-red-500"
+  >
+    <polyline points="23 18 13.5 8.5 8.5 13.5 1 6" />
+    <polyline points="17 18 23 18 23 12" />
+  </svg>
+</template>
