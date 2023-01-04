dbe60ef11f8d47de41a9cb3785b2e8ec14f40804
Sadegh Rastgoo <sadegh.rsg@gmail.com>
2023-01-04 17:03:04 +0330

    ✨ import/use/config Toast and Apexchart

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,30 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import Toast from "vue-toastification";
+import VueApexCharts from "vue3-apexcharts";
+import "./index.css";
+import "vue-toastification/dist/index.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+const options = {
+  position: "top-center",
+  toastClassName: "toastification--error",
+  timeout: 5000,
+  closeOnClick: true,
+  pauseOnFocusLoss: true,
+  pauseOnHover: false,
+  draggable: true,
+  draggablePercent: 0.6,
+  maxToasts: 1,
+  showCloseButtonOnHover: false,
+  hideProgressBar: true,
+  closeButton: "button",
+  icon: true,
+  rtl: false,
+};
+
+app.use(Toast, options);
+app.use(VueApexCharts);
+
+app.mount("#app");
