bdd60082190a6dfc781ae7179647b6c04c0f56ec
Sadegh Rastgoo <sadegh.rsg@gmail.com>
2023-01-04 16:59:33 +0330

    💄 add Inter font as defaukt sans font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,vue}"],
   theme: {
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
